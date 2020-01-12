aa0487ffbf0312bba6b2d9d99a6a05b9dd5f1de0
Gustavo Bautista <bgustavo81@gmail.com>
2020-01-11 21:18:57 -0500

    simple app:

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+
+// const bugRoutes = require('./routes/bugRoutes');
+// const commentRoutes = require('./routes/commentRoutes');
+// const userRoutes = require('./routes/userRoutes');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', "*");
+//     res.setHeader(
+//         'Access-Control-Allow-Methods',
+//         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+//     );
+//     res.setHeader('Access-Control-Allow-Headers', "*")
+//     res.setHeader("Content-Type", "application/json");
+//     next();
+// });
+
+// app.use('/', bugRoutes);
+// app.use('/', commentRoutes);
+// app.use('/', userRoutes);
+
+// app.use((error, req, res, next) => {
+//     console.log(error);
+//     const status = eror.stateCode || 500;
+//     const message = error.message;
+//     const data = error.data;
+//     res.status(status).json({ message: message, data: data });
+// })
+
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
-const bugRoutes = require('./routes/bugRoutes');
-const commentRoutes = require('./routes/commentRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "*");
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-    );
-    res.setHeader('Access-Control-Allow-Headers', "*")
-    res.setHeader("Content-Type", "application/json");
-    next();
-});
-
-app.use('/', bugRoutes);
-app.use('/', commentRoutes);
-app.use('/', userRoutes);
-
-app.use((error, req, res, next) => {
-    console.log(error);
-    const status = eror.stateCode || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({ message: message, data: data });
+app.get('/', (req, res, next) => {
+    res.send({ "message": "hello there" });
 })
 
 const PORT  = process.env.PORT || 5000;
