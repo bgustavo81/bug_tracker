d83658864ad2afe93fa8d9b6ccb1579576f31227
Gustavo Bautista <bgustavo81@gmail.com>
2020-01-25 12:40:44 -0500

    built

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ const commentRoutes = require('./routes/commentRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 app.use(
     cookieSession({
@@ -49,25 +46,25 @@ app.use('/', userRoutes);
 
 
 app.use((error, req, res, next) => {
-    console.log(error);
+    console.log("line 49 " + error);
     const status = error.stateCode || 500;
     const message = error.message;
     const data = error.data;
     res.status(status).json({ message: message, data: data });
 })
 
-if (process.env.NODE_ENV === 'production') {
-    // Express will serve up production assets 
-    // like our main.js file or main.css file
-    app.use(express.static('client/build'));
-
-    // Express will serve up the index.html file
-    // if it doesn't recognize the route
-    const path = require('path');
-    app.get("*", (req, res, next) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+if (process.env.NODE_ENV === "production") {
+    //express will serve up production assets like our //main.js file, or main.css file!!
+    app.use(express.static("client/build"));
+  
+    //server will serve up the index.html file if it does
+    //not recognize the route
+  
+    const path = require("path");
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
-}
+  }
 
 
 const PORT  = process.env.PORT || 5000;
