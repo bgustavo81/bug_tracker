51e9bbb45fef188ffb788016679635b6723a437f
Gustavo Bautista <bgustavo81@gmail.com>
2020-02-07 17:42:30 -0500

    fixed error

--- index.js
+++ index.js
@@ -66,10 +66,10 @@ app.use('/', surveyRoutes);
 
 if (process.env.NODE_ENV === "production") {
 //Set static folder
-app.use(express.static("/client/build"));
+app.use(express.static("client/build"));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build"));
 });
 }
 
